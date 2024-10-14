e1830f116be170b39320c563b67666f9cb20ac2c
khalilsen1991 <khalilsebastianenrique@gmail.com>
2024-10-14 13:07:19 -0300

    remove files

--- src/utils/CardsArrayGenerate/index.ts
+++
@@ -1,7 +0,0 @@
-import { ICard } from '../../types'
-import { getCardGuildUserController } from '../../controllers/cardGuildUser'
-
-export const CardsArrayGenerate = async (cards: ICard, userId: string, guildId: string) => {
-	const cardQuantity = await getCardGuildUserController(userId, cards.code, guildId) as ICard[]
-	return cardQuantity.length
-}
