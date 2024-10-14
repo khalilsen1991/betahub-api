5734371e302f76a31682bb4a1b7731068f8e5f2c
khalilsen1991 <khalilsebastianenrique@gmail.com>
2024-10-14 13:08:01 -0300

    remove files

--- src/test/guildmembers.test.ts
+++
@@ -1,8 +0,0 @@
-import supertest from 'supertest'
-import { app } from '..'
-
-describe('GET /guild-member/:memberId/guild/:guildId', () => {
-	it('should return guild member', async () => {
-		await supertest(app.callback()).get('/guild-member/1/guild/1').expect(404)
-	})
-})
--- src/test/index.ts
+++
@@ -1,8 +0,0 @@
-/* import { app } from '../index'
-import Router from 'koa-router'
-import supertest from 'supertest'
-
-export const testServer = (route: any) => {
-	route(app)
-	return supertest(app.listen())
-} */
